1eac5592126e990d9b590be17421a848bb738e3c
gvananya <gvananya9@gmail.com>
2025-09-16 12:58:53 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,14 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220,34,78);
+  rectMode(CENTER);
+  rect(200 ,200,70,80);
+  rect(200,140,30,40);
+  circle(150,150,40);
+  circle(250,150,40);
+  triangle(200,40,250,90,150,120);
+  
+}
