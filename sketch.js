46b38ca31172972d15dd082144e351867f516b79
gvananya <gvananya9@gmail.com>
2025-09-18 00:56:18 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,21 +1,14 @@
 function setup() {
   createCanvas(400, 400);
-
-  //RGB values
-  background(24, 0, 43);
-
-  //this is the brush
-  strokeWeight(5);
-  //colour of the brush
-  stroke(255, 255, 0);
 }
+
 function draw() {
-  let posY1 = mouseY;
-  let posX1 = mouseX;
-  point(posX1, posY1);
-  point(posX1 + 30, posY1 + 30);
-  point(posX1+40,posY1)
-  
-  
+  background(220,34,78);
+  rectMode(CENTER);
+  rect(200 ,200,70,80);
+  rect(200,140,30,40);
+  circle(150,150,40);
+  circle(250,150,40);
+  triangle(200,40,250,90,150,120);
   
 }
